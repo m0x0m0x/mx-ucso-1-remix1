ebf1f6497084161e3efc70f6037119caa273a296
m0x0m0x <mx0mx@tuta.io>
2025-04-10 09:26:44 +0530

    work

--- remixwork/s118.sol
+++ remixwork/s118.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: PantySmells 
 
+pragma solidity ^0.8.26;
 
 // ⠄⠄⠸⣿⣿⢣⢶⣟⣿⣖⣿⣷⣻⣮⡿⣽⣿⣻⣖⣶⣤⣭⡉⠄⠄⠄⠄⠄
 // ⠄⠄⠄⢹⠣⣛⣣⣭⣭⣭⣁⡛⠻⢽⣿⣿⣿⣿⢻⣿⣿⣿⣽⡧⡄⠄⠄⠄
@@ -13,4 +14,22 @@
 // ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠄⠄⠄⠄⢹⣿⣿⣿⣿⣿⣿⣿⣿⠁⠄
 // ⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⠄⠄⠄⠸⣿⣿⣿⣿⣿⡿⢟⣣⣀
 
+// Inbox Contract
+contract Inbox {
+    
+    // Set the varible of tyep string
+    string public message;
 
+    function InboxFunc(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+}
